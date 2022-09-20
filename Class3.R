630ebba106b441d1fcea434f276f2e5f60c86b4a
chhtwhc <45993330+chhtwhc@users.noreply.github.com>
2022-09-20 18:47:11 +0800

    Quick note for Class3

---
+++ Class3.R
@@ -0,0 +1,38 @@
+## Class 3 -- 2022/09/20 ##
+# 1. Common mistakes in homework: subset the list and dataframe($, [], [[]])
+# 2. Recap -- for-loop
+  # 2-1. Vector(mode, length) ; seq(from, to , by) ; length()
+  # 2-2. Syntax of for loop: 
+    # for (variable in vector) {
+    #   STATEMENT1
+    #   STATEMENT2
+    #   ...
+    # }
+  # 2-3. With and without {} in for-loop
+# 3. Recap -- functions
+  # 3-1. Syntax of function
+    # function(input1, input2, ...){
+    #   STATEMENT1
+    #   STATEMENT2
+    #   ...
+    #
+    #   return()
+    # }
+  # 3-2. Global and Local environment
+  # 3-2. How inputs work in function
+  # 3-3. How to set default input
+  # 3-4. Try to avoid using variables from global environment in function
+# 4. Nested for-loop
+  # 4-1. Which one should be the inner for-loop and which one should be the outer one.
+  # 4-2. matplot()
+  # 4-3. Tips to debug: Test the loop using small values and easy statement before starting time consuming analysis
+  # 4-4. Tips to debug: Manually loop
+# 5. Logical operators
+  # 5-1. What is logical operators: >, <, >=, <=, ==, !=, &, |(or)
+  # 5-2. Boolean (TRUE, FALSE): TRUE == 1, FALSE == 0
+  # 5-3. Subsetting using logical operators
+    # 5-3-1. Common mistakes: R recycles the vector
+
+
+
+
