06fcbf0272e9ecaf279f1180364fd00a7301d9e5
chhtwhc <45993330+chhtwhc@users.noreply.github.com>
2022-09-27 15:22:10 +0800

    Quick note for Class4

---
+++ Class4.R
@@ -0,0 +1,46 @@
+## Class 4 -- 2022/09/27 ##
+# 1. Quick overview of the homework
+# 2. Recap 
+  # 2-1. for-loop
+  # 2-2. nested for-loop
+    # 2-2-1. Coding style about using {} in loops
+  # 2-3. logical operators
+# 3. Probability
+  # 3-1. runif(x, min, max)
+  # 3-2. Calculate the probability using runif()
+# 4. if and if-else statement
+  # 4-1. Syntax of if  
+    # if (CONDITOIN){
+    #   STATEMENT1
+    #   STATEMENT2
+    #   ...
+    # } else {
+    #   STATEMENT1
+    #   STATEMENT2
+    # }
+  # 4-2. if (TRUE){} and if (False){}
+  # 4-3. Common mistake
+    # 4-3-1. else should follow the same line as curly bracket of if 
+    # 4-3-2. if can only take the first element
+  # 4-4. if-else statement in one line
+# 5. ifelse(test, yes, no)
+  # 5-1. Benefit of using ifelse() rather than if-else statement
+  # 5-2. Examples: Quickly produce a new column base on the condition
+# 6. While-loop
+  # 6-1. Syntax of if  
+    # While (CONDITOIN){
+    #   STATEMENT1
+    #   STATEMENT2
+    #   ...
+    # }
+  # 6-2. Keep updating value within while-loop
+  # 6-3. Forever-running while-loop
+# 7. Tidy and cleaned code
+# 8. Give an example as what can R do for you -- Demographic stochastic population growth
+  # 8-1. The logic of coding while coding
+  # 8-2. Debug skills
+
+
+
+
+
